2d81dbdd3b3b97c51621cefe032127480cf34988
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2023-12-06 13:46:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main()
 {
+	vector<Piece> pieces;
+
 	Piece sallePrincipale("Salle principale", "Premiere piece en entrant");
 	Piece couloirGauche("Couloir Gauche", "Connecte au donjon et Tableau de bombes"); //Porte gauche
 	Piece salleBombe("Salle avec un Tableau avec des Bombes", "Cul-de-sac"); //Tableau bombe
@@ -17,11 +19,11 @@ int main()
 	Piece donjon("Donjon", "Portail vers le niveau de lave"); //Niveau lave
 	Piece couloirDroit("Couloir Droit", "Connecte au tableau d'eau, cour arriere et tableau de neige"); //Porte droite
 	Piece courArriere("Cour Arriere", "Cul-de-sac");
-	Piece dehors("Dehors", "¿ l'exterieur du ch‚teau"); //Salle initiale
+	Piece dehors("Dehors", "√Ä l'exterieur du ch√¢teau"); //Salle initiale
 	Piece escaliers("En haut des escaliers", "Debut du deuxieme etage");
 	Piece horloge("Salle avec une Horloge Geante", "Portail vers horloge"); //Tableau horloge
 	Piece exposition("Salle d'exposition", "Salle avec une statue");
-	//Piece courArriere("Cour arriere"); On a dej‡ une cour arriere
+	//Piece courArriere("Cour arriere"); On a dej√† une cour arriere
 	Piece infini("Escalier infini", "Escalier interminable jusqu'au boss");
 	Piece bossFinal("Boss final", "Bowser");
 
@@ -39,6 +41,23 @@ int main()
 	escaliers.connecterPieces(exposition, "Ouest");
 	infini.connecterPieces(bossFinal, "Nord");
 
+	Carte carte(sallePrincipale, pieces);
+
+	carte.ajouterPiece(sallePrincipale);
+	carte.ajouterPiece(couloirGauche);
+	carte.ajouterPiece(couloirDroit);
+	carte.ajouterPiece(escaliers);
+	carte.ajouterPiece(infini);
+	carte.ajouterPiece(horloge);
+	carte.ajouterPiece(exposition);
+	carte.ajouterPiece(bossFinal);
+	carte.ajouterPiece(courArriere);
+	carte.ajouterPiece(aquarium);
+	carte.ajouterPiece(salleEnneigee);
+	carte.ajouterPiece(donjon);
+	carte.ajouterPiece(salleBombe);
+	carte.ajouterPiece(dehors);
+
 	cout << "Welcome to Super Marios 64" << endl; //Nom du jeu
 	cout << endl;
 	cout << "Controles sont :" << endl;
@@ -48,19 +67,97 @@ int main()
 	cout << '\t' << "O pour aller a l'Ouest de la piece ou vous etes en ce moment." << endl;
 	cout << '\t' << "START pour commencer le jeu" << endl;
 	cout << '\t' << "EXIT pour sortir du jeu" << endl;
-	cout << "Type START to start the game" << endl;
 
-	cout << "----" << sallePrincipale.getNom() << "----" << endl; //Piece initiale
 	string inputMovement;
+
+	while (inputMovement != "START")
+	{
+		cout << "Type START to commence the game : ";
+		cin >> inputMovement;
+		if (inputMovement == "EXIT")
+		{
+			break;
+		}
+	}
 
 	while (inputMovement != "EXIT")
 	{
+		cout << endl;
+		cout << "----" << carte.getSalleActuelle()->getNom() << "----" << endl;
+
+		cout << "Your choices are :" << endl;
+		
+		if (carte.getSalleActuelle()->getNord() != nullptr)
+		{
+			cout << '\t' << "N:" << carte.getSalleActuelle()->getNord()->getNom() << endl;
+		}
+
+		if (carte.getSalleActuelle()->getSud() != nullptr)
+		{
+			cout << '\t' << "S:" << carte.getSalleActuelle()->getSud()->getNom() << endl;
+		}
+		
+		if (carte.getSalleActuelle()->getEst() != nullptr)
+		{
+			cout << '\t' << "E:" << carte.getSalleActuelle()->getEst()->getNom() << endl;
+		}
+		
+		if (carte.getSalleActuelle()->getOuest() != nullptr)
+		{
+			cout << '\t' << "W:" << carte.getSalleActuelle()->getOuest()->getNom() << endl;
+		}
+
 		cout << "Type your next move : ";
 		cin >> inputMovement;
+		cout << endl;
+
+		if (inputMovement == "N")
+		{
+			if (carte.getSalleActuelle()->getNord() != nullptr)
+			{
+				carte.setSalleActuelle(*(carte.getSalleActuelle()->getNord()));
+			}
+			else
+			{
+				cout << "You cannot go North of where you are right now." << endl;
+			}
+		}
+		else if (inputMovement == "S")
+		{
+			if (carte.getSalleActuelle()->getSud() != nullptr)
+			{
+				carte.setSalleActuelle(*(carte.getSalleActuelle()->getSud()));
+			}
+			else
+			{
+				cout << "You cannot go South of where you are right now." << endl;
+			}
+		}
+		else if (inputMovement == "W")
+		{
+			if (carte.getSalleActuelle()->getOuest() != nullptr)
+			{
+				carte.setSalleActuelle(*(carte.getSalleActuelle()->getOuest()));
+			}
+			else
+			{
+				cout << "You cannot go West of where you are right now." << endl;
+			}
+		}
+		else if (inputMovement == "E")
+		{
+			if (carte.getSalleActuelle()->getEst() != nullptr)
+			{
+				carte.setSalleActuelle(*(carte.getSalleActuelle()->getEst()));
+			}
+			else
+			{
+				cout << "You cannot go East from where you are right now." << endl;
+			}
+		}
+		else
+		{
+			cout << "Please type a valid movement input." << endl;
+		}
 	}
-
-
-
-
-
 }
