3b319b80bb7cd0a629929bba5ae6d01f7af16e28
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2023-12-18 10:49:09 -0500

    Update Tableau.cpp

--- Tableau.cpp
+++ Tableau.cpp
@@ -1,11 +1,22 @@
-#include "Tableau.cpp"
+#include "Tableau.hpp"
 
-Tableau::visiterTableau(){
-    if (nombreEtoiles > 0){
+Tableau::Tableau(string nomObjet, string descriptionObjet, int etoiles)
+{
+    nom = nomObjet;
+    description = descriptionObjet;
+    nombreEtoiles = etoiles;
+}
+
+Tableau::~Tableau() {}
+
+int Tableau::visiterTableau() {
+    if (nombreEtoiles > 0) {
         nombreEtoiles -= 1;
+        return nombreEtoiles;
     }
     else {
-        cout << "Vous avez déjà "
+        cout << "Vous avez déjà toutes les étoiles de ce tableau";
+        return 0;
     }
 
 }
