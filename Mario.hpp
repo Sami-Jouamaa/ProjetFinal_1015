34fbab0c151d52422dfc690a47646f598c3e56fc
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2023-12-19 10:43:58 -0500

    Update Mario.hpp

--- Mario.hpp
+++ Mario.hpp
@@ -8,12 +8,15 @@ class Mario
 {
 
 public:
-    Mario();
+    Mario(bool aYoshi, int etoilesPossedees);
     ~Mario();
 
     void wahoo();
 
+    void ajouterObjet(Objet objet);
     void gagnerEtoile();
+    int getNbreEtoilesMario();
+    void setNbreEtoilesMario(int etoiles);
 
 private:
     vector<Objet> objets;
